203680959d5e47a7b19873c159df400e80acd23b
Isaac <isaac778@ymail.com>
2023-06-23 16:39:49 -0700

    Updating file

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(10):
-        if (j > i) != 89:
-            print("{:02d}, ".format(i+j), end='')
-        else:
-            print("{:02d}".format(i+j))
+    for j in range(1, 10):
+        if (j > i):
+            print(i, end='')
+            print(j, end='')
+            if (j>i and i!= 8):
+                print(", ", end='')
+            else:
+                print("")
