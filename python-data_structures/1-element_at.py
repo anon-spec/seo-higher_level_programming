34e0af3c7250d226f8b67ee461a320927bb2522f
Isaac <isaac778@ymail.com>
2023-06-26 20:01:07 -0700

    Updating file

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     for i in my_list:
         if idx < 0:
             return None
-        elif idx > range(my_list):
+        elif idx > len(my_list):
             return None
         else:
             print("{}".format(idx))
