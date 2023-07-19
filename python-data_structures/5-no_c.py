1a83484cdd31ac8c13973f99fa21919c4700a7cc
Isaac <isaac778@ymail.com>
2023-07-19 15:18:02 -0700

    Updating file

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     newString = ""
     for char in my_string:
         if char != 'c' and char != 'C':
-            newString = char
+            newString = newString + char
     return newString
     return my_string
