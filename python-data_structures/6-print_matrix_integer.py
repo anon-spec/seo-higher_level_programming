f7c5c5aaac5ef785e7776644e017a3486f174456
Isaac <isaac778@ymail.com>
2023-08-01 15:48:16 -0700

    Updating file

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        row_str = [str(item) for item in row]
-        print(" ".join(row_str))
+    for row in range(len(matrix)):
+        sub_len = len(matrix[i])
+        for column in range(sub_len):
+            if column != sub_len - 1:
+                end_char = ' '
+            else:
+                end_char = ''
+            print("{:d}".format(matrix[row][column]), end=end_char)
+        print("")
