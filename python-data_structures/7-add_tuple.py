dc189e12bef93830674114a1e0d79e7fd45dee26
Isaac <isaac778@ymail.com>
2023-08-01 16:38:42 -0700

    updating file

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -9,7 +9,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a = tuple_a[0]
         b = tuple_a[1]
     if len(tuple_b) == 0:
-        c, d == (0, 0)
+        c, d = (0, 0)
     elif len(tuple_b) == 1:
         c = tuple_b[0]
         d = 0
