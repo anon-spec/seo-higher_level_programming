9bfdecc2637e26ed09e066c32cc861e2247090f7
Isaac <isaac778@ymail.com>
2023-08-03 19:52:55 -0700

    Adding file to directory

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    maxVal = my_list[0]
+    for i in my_list:
+        if my_list[i] > myVal:
+            maxVal = my_list[i]
+    return maxVal
