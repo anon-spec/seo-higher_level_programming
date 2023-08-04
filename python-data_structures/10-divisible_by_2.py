4b1a1f882603220903301684de038ce39c7f2d39
Isaac <isaac778@ymail.com>
2023-08-03 21:52:13 -0700

    Updating file

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     newList = my_list
     for i in range(len(my_list)):
-        if my_list[i] % 2:
+        if (my_list[i] / 2) % 2:
             return True, newList
         else:
             return False, newList
