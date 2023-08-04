915e7a24f04ee585f647286c824c0bf7adf9b468
Isaac <isaac778@ymail.com>
2023-08-04 15:03:26 -0700

    Updatig file

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    newList = my_list
-    for i in range(len(my_list)):
-        if (my_list[i] / 2) % 2:
-            return True, newList
-        else:
-            return False, newList
+    newList = [True if x % 2 == 0 else False for x in my_list]
+    return newList
