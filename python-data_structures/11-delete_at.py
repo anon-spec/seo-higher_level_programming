9bc89e7bee696bfde363c8b8d1a01b615c81e9cd
Isaac <isaac778@ymail.com>
2023-08-04 15:24:42 -0700

    Adding file

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if (idx < 0) or (idx > len(my_list)):
+        return my_list
+    else:
+        newList = my_list
+        del newList[idx]
+        return newList
