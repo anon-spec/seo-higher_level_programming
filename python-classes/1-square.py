379419e01ae6c35a843e73c66dbd183bfac9abb1
Isaac <isaac778@ymail.com>
2023-08-09 14:48:38 -0700

    Adding file

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Example of docstrings for module"""
+
+
+class Square:
+    """class Square defined with private attribute size"""
+    def __init__(self, size):
+        """initializing object size"""
+        self.__size = size
