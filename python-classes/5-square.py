4a955ef20affd606697f8237af03584545c2c46b
Isaac <isaac778@ymail.com>
2023-08-10 10:20:31 -0700

    Updating file

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -33,8 +33,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in (range(len(self.__size)) - 1):
-                for j in (range(len(self.__size)) - 1):
+            for i in (range(self.__size) - 1):
+                for j in (range(self.__size) - 1):
                     print("#", end='')
                 print()
                 j = 0
