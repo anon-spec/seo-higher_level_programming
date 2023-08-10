205fe2f227051b2457495ca88bbe09c8dcb84e87
Isaac <isaac778@ymail.com>
2023-08-10 13:22:15 -0700

    Updating fiel

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -48,9 +48,12 @@ class Square:
     @position.setter
     def position(self, value):
         """Property setter"""
-        if len(position) != 2 or type(value) is not tuple or type(value[0]) is not tuple:
+        if len(value) != 2 or type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value[1]) is not tuple or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
+
