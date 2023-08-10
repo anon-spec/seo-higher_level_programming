ec57be13d096378d5df2117ffd16a5781075194e
Isaac <isaac778@ymail.com>
2023-08-10 13:19:25 -0700

    Updating file

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Example of docstrings for module"""
+"""Example of docstrings for module
+"""
 
 
 class Square:
@@ -47,8 +48,9 @@ class Square:
     @position.setter
     def position(self, value):
         """Property setter"""
-        if len(position) != 2 or type(value) is not tuple or type(value[0]) is not tuple or 
-        type(value[1]) is not tuple or value[0] < 0 or value[1] < 0:
+        if len(position) != 2 or type(value) is not tuple or type(value[0]) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[1]) is not tuple or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
