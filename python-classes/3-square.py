4947b6d3082aa2371c479017a4d7bb8d35a7316c
Isaac <isaac778@ymail.com>
2023-08-09 18:19:00 -0700

    Adding file

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Example of docstrings for module"""
+
+
+class Square:
+    """class Square defined with private attribute size"""
+    def __init__(self, size=0):
+        """initializing object size"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """finding area of a square"""
+        return (self.__size * self.__size)
