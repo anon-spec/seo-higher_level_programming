61689916cb181a67b6135529878d05b75148067f
Isaac <isaac778@ymail.com>
2023-08-09 19:08:41 -0700

    Updating file

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -18,7 +18,7 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        elif value >= 0:
+        if value >= 0:
             self.__size = value
 
     def area(self):
