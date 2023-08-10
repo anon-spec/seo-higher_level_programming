a4ebfdd8512d8efeb094c024feb249fef38d66c7
Isaac <isaac778@ymail.com>
2023-08-10 13:37:06 -0700

    Updating file

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -51,9 +51,9 @@ class Square:
         """Property setter"""
         if len(value) != 2 or type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value[1]) is not tuple or value[0] < 0 or value[1] < 0:
+        elif type(value[1]) is not int or type(value[0]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value[0]) is not tuple:
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
