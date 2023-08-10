bb116febf1ac16425238e7281fae9b7cc776ec8e
Isaac <isaac778@ymail.com>
2023-08-09 19:28:22 -0700

    Updating file

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,7 +10,7 @@ class Square:
 
     def __init__(self, size=0):
         """initializing object size"""
-        self.size = size
+        self.__size = size
 
     def size(self, value):
         """Property setter"""
