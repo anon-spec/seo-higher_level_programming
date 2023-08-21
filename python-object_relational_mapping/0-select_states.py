f1138a8bc8c148b9ccca7e015d4c1317175a2ef1
Isaac <isaac778@ymail.com>
2023-08-21 16:27:53 -0700

    Updating file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """MODULE DOCSTRING"""
 
+
 import sys
 import MySQLdb
 
-def command(argument):
-    db = MySQLdb.connect(host = "localhost",
-                        user = argument[0],
-                        password = argument[1], 
-                        dbName = argument[2],
-                        port = "3306")
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                        user=argument[0],
+                        password=argument[1],
+                        dbName=argument[2],
+                        port="3306")
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
