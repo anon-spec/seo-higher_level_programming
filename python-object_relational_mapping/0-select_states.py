803febbe8eccf8ddc9c32fb303a0d5034e4b56f2
Isaac <isaac778@ymail.com>
2023-08-21 17:21:55 -0700

    Updating file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """MODULE DOCSTRING"""
 
-
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", 
+    db = MySQLdb.connect(host="localhost",
                          user=sys.argv[1],
                          password=sys.argv[2],
                          dbName=sys.argv[3],
